b461fd975f2a1c2bec6feb8c19b87448976df83a
yeremygarcia <jeremyisgarcia@gmail.com>
2023-04-05 22:42:17 -0400

    1

--- email.js
+++ email.js
@@ -1,14 +1,28 @@
 // items on email landing page
-// Gmail - inbox - sent - drafts - trash -- contacts - jim - bob - mike -- emails
 
-let gMail = {
-  mailboxes: ["inbox", "sent", "drafts", "trash"],
+let appData = {
+  name: "gmail",
+  mailboxes: ["inbox", "outbox", "drafts", "trash"],
   contacts: [
     { name: "Jim", email: "jim@gmail.com", lastMessage: "Hi" },
     { name: "John", email: "john@gmail.com", lastMessage: "Hello" },
     { name: "Mike", email: "mike@gmail.com", lastMessage: "Cheers" },
     { name: "Bob", email: "bob@gmail.com", lastMessage: "Bye" },
   ],
-};
 
-console.log(gMail.mailboxes[1]);
+  inbox: [{ from: "Jim" }, { from: "John" }, { from: "Mike" }, { from: "Bob" }],
+
+  outbox: [
+    { sent: "Jim" },
+    { sent: "John" },
+    { sent: "Mike" },
+    { sent: "Bob" },
+  ],
+
+  drafts: [
+    { from: "Jim" },
+    { from: "John" },
+    { from: "Mike" },
+    { from: "Bob" },
+  ],
+};
