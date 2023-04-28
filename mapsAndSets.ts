fc790e71e30b79c0d1d0358a0710f4716e56cc3b
yeremygarcia <jeremyisgarcia@gmail.com>
2023-04-28 01:11:10 -0400

    1

--- mapsAndSets.ts
+++ mapsAndSets.ts
@@ -1,5 +1,6 @@
+
 // create the fruitInventory map
-let fruitInventory = new Map([
+let fruitInventory: Map<string, number> = new Map([
   ["apple", 10],
   ["banana", 20],
   ["orange", 30],
@@ -9,13 +10,13 @@ let fruitInventory = new Map([
 fruitInventory.set("apple", 15);
 
 // get the 'banana' inventory and store it in a variable
-let bananaInventory = fruitInventory.get("banana");
+let bananaInventory: number | undefined = fruitInventory.get("banana");
 
 // create the uniqueColors set
-let uniqueColors = new Set(["red", "blue", "green"]);
+let uniqueColors: Set<string> = new Set(["red", "blue", "green"]);
 
 // add the color 'yellow' to the uniqueColors set
 uniqueColors.add("yellow");
 
 // check if 'blue' is in the uniqueColors set and store the result in a variable
-let hasBlue = uniqueColors.has("blue");
+let hasBlue: boolean = uniqueColors.has("blue");
